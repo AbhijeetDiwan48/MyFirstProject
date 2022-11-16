61faf5be315e9d6c5f09e9986e125718a04597de
Abhijeet <Abhijeet@192.168.1.216>
2022-11-16 21:08:22 +0530

    My First Project Commit & Push

---
+++ CucumberFramework/src/test/java/theory.java
@@ -0,0 +1,25 @@
+
+public class theory {}
+/*
+ Why
+ BDD - focusing on what to test not on how to test
+  
+     
+     
+     
+ File
+ - StepDefination (package)
+    - StepDefination
+ - Test runner (package)
+ Folder
+ - Feature
+ Maven Dependancies
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
